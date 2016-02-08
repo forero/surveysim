f0054bd4e3289ed73ed8bb84ee5ff140ade34d9b
forero <j.e.forero.romero@gmail.com>
2016-02-08 11:13:49 -0500

    initial setup

---
+++ py/__init__.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+=========
+surveysim
+=========
+Tools to simulate the DESI survey
+"""
+from __future__ import absolute_import
+from . import assignfiber
+from . import mock
+from . import mtl
+from . import quickcat
+from ._version import __version__
+
